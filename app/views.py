a965ab37f46999ad0417bd739f8cb18696115c8a
deadbladex <deadbladex@hotmail.com>
2017-11-07 20:54:53 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -62,15 +62,16 @@ def cadastro_cursos(request):
             'year':datetime.now().year,
         })
     )
+
 def cadastro_vestibulares(request):
     assert isinstance(request, HttpRequest)
     return render(
           request,
           'app/cadastro_vestibulares.html',
           context_instance = RequestContext(request,
-        {
+          {
             'title':'Cadastro de vestibulares',
             'vestibulares': Vestibular.objects.all(),
             'year':datetime.now().year,
-        })
+          })
     )
